833c123339e1e2e20465520a029dd09f40c4001d
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-07-06 09:45:49 -0500

    test: prod

--- tests/route_test.py
+++ tests/route_test.py
@@ -1,13 +1,14 @@
 import requests
 
-BASE_URL = "http://44.214.216.202:8080"
-
+#BASE_URL = "http://44.214.216.202:8080"
+BASE_URL = "http://44.194.87.132:8080"#prod
 login_data = {
-    "User_mail": "allan",  
+    "User_mail": "allanprod2",  
     "password": "1234"                
 }
 
-login_response = requests.post(f"http://52.203.72.116:8080/login", json=login_data)
+#login_response = requests.post(f"http://52.203.72.116:8080/login", json=login_data)
+login_response = requests.post(f"http://100.25.74.174:8080/login", json=login_data)#prod
 
 if login_response.status_code != 200:
     print("Error:", login_response.status_code, login_response.json())
@@ -18,7 +19,7 @@ print("Token :", token)
 
 # endpoint edit
 update_data = {
-    "Description": "Changed description",
+    "Description": "Changed description by prod in te video",
     #"Id_preferences": 2,
     #"Id_type": 1
 }
